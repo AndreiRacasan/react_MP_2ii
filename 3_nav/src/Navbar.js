75ba90197e0d55fc274caa4d849c17a0eb320753
Andrei Racasan <aracasan@outlook.com>
2021-01-12 22:34:16 +0000

    initial commit - third project: nav (i)

---
+++ 3_nav/src/Navbar.js
@@ -0,0 +1,37 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { FaBars, FaGithub } from 'react-icons/fa'
+import { links, social } from './data'
+import logo from './logo.svg'
+
+const Navbar = () => {
+  return <nav>
+    <div className="nav-center">
+      <div className="nav-header">
+        <img src={logo} alt="logo"/>
+        <button className="nav-toggle">
+          <FaBars />
+        </button>
+      </div>
+      <div className="links-container show-container">
+        <ul className="links">
+          {links.map((link) => {
+            const {id, url, text} = link;
+            return <li key={id}>
+              <a href={url}>{text}</a>
+            </li>
+          })}
+        </ul>
+      </div>
+      <ul className="social-icons">
+        {social.map((socIco) => {
+          const {id, url, icon} = socIco;
+          return <li key={id}>
+            <a href={url}>{icon}</a>
+          </li>
+        })}
+      </ul>
+    </div>
+  </nav>
+}
+
+export default Navbar
